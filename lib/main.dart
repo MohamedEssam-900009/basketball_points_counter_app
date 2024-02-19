60050979c7880a4ebba37aa5d09f53fbd733c880
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-02-19 23:06:06 +0200

    adding text

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,18 @@ class PointsCounter extends StatelessWidget {
           title: const Text('Points Counter'),
           backgroundColor: Colors.orange,
         ),
+        body: const Column(
+          children: [
+            Text(
+              'Team A',
+              style: TextStyle(fontSize: 32),
+            ),
+            Text(
+              '0',
+              style: TextStyle(fontSize: 150),
+            ),
+          ],
+        ),
       ),
     );
   }
