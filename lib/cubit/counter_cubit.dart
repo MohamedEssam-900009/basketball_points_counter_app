879237973f65bf95eb340b809af99e2e26311dc2
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-05-04 03:02:21 +0300

    fix all imports

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,5 +1,6 @@
-import 'package:basketball_points_counter_app/cubit/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterIncrementState());
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:basketball_points_counter_app/cubit/counter_cubit.dart';
+import 'cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
